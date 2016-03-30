feaaef0e47b7f18f4fc55bdab8a0ca5ff648bafd
marrific <marcela.nowak@nyu.edu>
2016-03-30 13:24:08 -0400

    First commit

---
+++ Empty/sketch.js
@@ -0,0 +1,38 @@
+var capture;
+var w = 640,
+  h = 480;
+
+function setup() {
+  capture = createCapture(VIDEO);
+  createCanvas(w, h);
+  capture.size(w, h);
+  capture.hide();
+}
+
+function draw() {
+  image(capture, 0, 0, w, h);
+  capture.loadPixels();
+ if (capture.pixels.length > 0) {
+  var h = capture.height;
+  var w = capture.width;
+  var total = 0;
+  var n = 0;
+  var i = 0;
+  for (var y = 0; y < h; y++) {
+    for (var x = 0; x < w; x++) {
+      var r = capture.pixels[i];
+      total += r;
+      n++;
+      i+= 4;
+    }
+
+  }
+
+  var avg = total / n;
+  select('#average').elt.innerText = avg;
+
+
+  // don't forget this!
+
+  }
+}
